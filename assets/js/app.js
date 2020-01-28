12a2bc97b5eb4cd01287f3ad693e053b09a3c342
COR1999 <cian.orourke@gmail.com>
2020-01-28 17:35:06 +0000

    got api working with my input feild and sort button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,129 @@
 let baseUrl = "http://ws.audioscrobbler.com"
 let apiKey = "977e2d82b14fe34163d15b95f2c1ce55"
+let dropdownValue;
 
 
 
 $(document).ready(function () {
 
+    // artistSearch = document.querySelector("artist").selected;
+    // console.log(artistSearch);
+    let selectBar = document.getElementsByClassName("custom-select");
+    $("select").each(function () {
+        var $this = $(this), numberOfOptions =
+            $(this).children("option").length;
+    })
+    // // console.log(input.value.toString());
+
+    /*Dropdown Menu*/
+    $('.dropdown').click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+        $(this).find('.dropdown-menu').slideToggle(300);
+    });
+    $('.dropdown').focusout(function () {
+        $(this).removeClass('active');
+        $(this).find('.dropdown-menu').slideUp(300);
+    });
+    // let dropdownValue;
+    $('.dropdown .dropdown-menu li').click(function () {
+        dropdownValue = "";
+        $(this).parents('.dropdown').find('span').text($(this).text());
+        // $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+        // console.log($(this).attr("id"));
+        dropdownValue = $(this).attr("id");
+        // console.log(dropdownValue);
+        searchTheAPI(dropdownValue);
+    });
+    /*End Dropdown Menu*/
+})
+
+function searchTheAPI(dropdownValue) {
     $("#search").on("click", function () {
         // Artist Search 
-        let input = $("#myInput").val();
-        console.log(input);
+        // console.log(input);
+        let input = "";
+        let searchType = dropdownValue;
+        input = $("#myInput").val();
+        if (searchType === "artist") {
+            $.ajax({
+                "async": true,
+                "crossDomain": true,
+                "url": `${baseUrl}/2.0/?method=artist.search&artist=${input}&api_key=${apiKey}&limit=10&format=json`,
+                "method": "GET",
+            }).then(function (response) {
+                console.log(response.results.artistmatches.artist[0])
+                let artistArray = response.results.artistmatches.artist;
+                let newArtistArray = [];
+                console.log(response.results)
+                artistArray.forEach(function (artist) {
+                    newArtistArray.push(`<img src="${artist.image[1]["#text"]}"><h4>${artist.name}</h4><p></p>`)
+                    console.log(artist);
+                })
+                // console.log(newAlbumArray);
+                $("#artist-list").html(newArtistArray);
+
+            })
+        } else if (searchType === "album") {
+            $.ajax({
+                "async": true,
+                "crossDomain": true,
+                "url": ` ${baseUrl}/2.0/?method=album.search&album=${input}&api_key=${apiKey}&limit=10&format=json`,
+                "method": "GET",
+            }).then(function (response) {
+                console.log(response)
+                let albumArray = response.results.albummatches.album;
+                let newAlbumArray = [];
+                albumArray.forEach(function (album) {
+                    let albumImage = `<img src="${album.image[1]["#text"]}">`
+                    // console.log(album.artist)
+                    if (album.image[1]["#text"] === "") {
+                        console.log(album.artist);
+                        albumImage = "<h2>Cant retrieve Image</h2>"
+                    }
+                    newAlbumArray.push(`${albumImage}<h4>${album.artist}</h4><p>${album.name}</p>`)
+                    // console.log(album);
+                })
+                // console.log(newAlbumArray);
+                $("#album-list").html(newAlbumArray);
+            })
+        } else if (searchType === "song") {
+            $.ajax({
+                "async": true,
+                "crossDomain": true,
+                "url": ` ${baseUrl}/2.0/?method=track.search&track=${input}&api_key=${apiKey}&limit=10&format=json`,
+                "method": "GET",
+            }).then(function (response) {
+                let trackArray = response.results.trackmatches.track;
+                let newTrackArray = [];
+                trackArray.forEach(function (track) {
+                    newTrackArray.push(`<h4>${track.name}</h4><p>${track.artist}</p>`)
+                })
+                $("#track-list").html(newTrackArray);
+                // console.log(response.results.trackmatches.track);
+            })
+        }
+    })
+    $("#chart").on("click", function () {
         $.ajax({
             "async": true,
             "crossDomain": true,
-            "url": `${baseUrl}/2.0/?method=artist.search&artist=${input}&api_key=${apiKey}&format=json`,
+            "url": `${baseUrl}/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`,
             "method": "GET",
         }).then(function (response) {
-            console.log(response.results.artistmatches.artist[0])
+            console.log(response.tracks.track);
         })
-
     })
 
-    // // console.log(input.value.toString());
 
-
-})
-// Material Select
+}
 
 
 
 
-// let baseUrl = "https://deezerdevs-deezer.p.rapidapi.com/"
-// let searchUrl = "https://deezerdevs-deezer.p.rapidapi.com/artist/"
+
+                    // let baseUrl = "https://deezerdevs-deezer.p.rapidapi.com/"
+                    // let searchUrl = "https://deezerdevs-deezer.p.rapidapi.com/artist/"
 
 
 
