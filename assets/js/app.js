2c1e0745afec4b6d9cf0b50a1d9ce6d0abf173a6
COR1999 <cian.orourke@gmail.com>
2020-02-06 15:43:48 +0000

    added bit of styleing to charts and home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,6 @@ let submit = document.getElementById('search'),
 let objJson;
 
 $(document).ready(function () {
-    getCharts()
     $(function () {
         if ($("body").is(".chart")) {
             getCharts()
@@ -54,9 +53,6 @@ function GetSelectedValue() {
 
 
 function searchTheAPI(result) {
-    // $("#artist-list").empty()
-    // $("#album-list").empty()
-    // $("#track-list").empty()
     let input;
     let searchType = result;
     console.log(searchType);
@@ -145,7 +141,7 @@ function apiUrl(searchType, input) {
         case "chart":
             searchBy = "chart.gettoptracks";
             input = "";
-            limit = "125"
+            limit = "100"
 
             break;
         default:
@@ -162,21 +158,6 @@ function apiUrl(searchType, input) {
             "method": "GET",
         }))
 }
-
-//https://codepen.io/cristinaconacel/pen/ePVMME
-
-
-// function albumAndArtist(album) {
-//     $.ajax({
-//         "async": true,
-//         "crossDomain": true,
-//         "url": `${baseUrl}/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${album.artist}&album=${album.name}&format=json`,
-//         "method": "GET",
-//     }).then(function (data) {
-//         console.log(data);
-//     })
-// }
-
 
 
 function getSongs(album, index) {
@@ -230,26 +211,19 @@ function getTableElement(el, value) {
 }
 function chartHeaders() {
     let tableHeader = ""
-    var tableHeaders = [];
     let tableNumber = `<th scope="col">Number</th>`,
-        tableSong = `<th scope="col"> Song Name</th>`,
-        tableArtist = `<th scope="col">Artist Name</th>`,
-        tablePlaycount = `<th scope="col">Playcount</th>`;
+        tableSong = `<th scope="col">Song</th>`,
+        tableArtist = `<th scope="col">Artist</th>`,
+        tablePlaycount = `<th scope="col">Play count</th>`;
 
 
     tableHeader = tableNumber + tableSong + tableArtist + tablePlaycount
-    // tableHeaders.push(`<td>Number</td>`)
-    // tableHeaders.push(`<td>Song Name</td>`);
-    // tableHeaders.push(`<td>Artist Name</td>`);
-    // tableHeaders.push(`<td>Playcount</td>`);
-    // tableHeaders.push(`<td>Description</td>`);
 
     return `<thead><tr>${tableHeader}</tr></thead>`;
 }
 function chartRows(obj) {
     let tableRows = [];
     obj.forEach(function (track, index) {
-
         let dataRow = [];
 
         let newIndex = (getTableElement(index + 1));
@@ -276,7 +250,6 @@ function chartRows(obj) {
     // console.log(tableRows.length)
     let pageSize = 10;
     let totalPages = Math.ceil(tableRows.length / pageSize)
-
     while (setOfPages.length < totalPages) {
         setOfPages.push(tableRows.splice(0, pageSize))
     }
@@ -284,40 +257,6 @@ function chartRows(obj) {
     console.log(setOfPages)
     // return tableRows.join("");
     return setOfPages
-    // console.log(paginate(tableRows))
-    // if (tableRows.length === paginate(tableRows)) {
-    //     console.log(this)
-    // }
-    // let test = `
-    //     <h1>paginate(100)</h1>
-    //     ${JSON.stringify(paginate(tableRows))}
-    //     ${(this.name)}
-
-    // `
-    // let pagIn = paginate(tableRows)
-    // let page = pagIn.setOfPages;
-    // console.log((paginate(tableRows)))
-    // console.log(page)
-    // let finalPage = getButtons(pagIn);
-    // console.log(finalPage)
-
-    // document.getElementById("btn_next").addEventListener("click", function () {
-    //     if (finalPage < 10) {
-    //         finalPage++;
-    //         return finalPage
-    //         console.log(page[finalPage])
-    //         // console.log(obj)
-
-    //     }
-    // });
-    // document.getElementById("btn_prev").addEventListener("click", function () {
-    //     if (finalPage > 0) {
-    //         finalPage--;
-    //         return finalPage;
-    //         console.log(page[finalPage])
-    //     }
-    // });
-    // return page[finalPage].join("")
 
 }
 
@@ -356,7 +295,6 @@ function buildTable(rows, page) {
 
 }
 
-
 function turnSec(s) {
     // let minutes = parseInt(s / 60) % 60;
     // let seconds = s % 60;
@@ -377,10 +315,10 @@ function pageButtons(pages, rows) {
     let wrapper = document.getElementById("buttons")
     wrapper.innerHTML = ""
     for (var page = 1; page <= pages; page++) {
-        wrapper.innerHTML += `<button value="${page}" class="page btn" > ${page}</button > `
+        wrapper.innerHTML += `<button value="${page}" class="page-button" >${page}</button> `
     }
 
-    $(".page").on("click", function () {
+    $(".page-button").on("click", function () {
         $(".top-charts").empty()
         value = $(this).val()
         // console.log(value)
@@ -390,152 +328,4 @@ function pageButtons(pages, rows) {
     })
 };
 
-
-
-
-https://jasonwatmore.com/post/2018/08/07/javascript-pure-pagination-logic-in-vanilla-js-typescript
-
-function paginate(totalItems) {
-
-
-    // console.log(setOfPages)
-    // totalItems.forEach(function (item) {
-    //     console.log(item)
-    //     setOfPages.push(item)
-    // })
-    let currentPage = 0,
-        pageSize = 10,
-        maxPages = 10
-    // calculate total pages
-    let totalPages = Math.ceil(totalItems.length / pageSize);
-
-
-    let setOfPages = [];
-    while (setOfPages.length < totalItems.length) {
-        setOfPages.push(totalItems.splice(0, pageSize))
-    }
-    // console.log(setOfPages)
-
-    // ensure current page isn't out of range
-    if (currentPage) {
-        currentPage = 0;
-    } else if (currentPage > totalPages) {
-        currentPage = totalPages;
-    }
-
-    let startPage = 1,
-        endPage = totalItems / 10;
-    if (totalPages <= maxPages) {
-        // total pages less than max so show all pages
-        startPage = 1;
-        endPage = totalPages;
-    } else {
-        // total pages more than max so calculate start and end pages
-        let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
-        let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
-        if (currentPage <= maxPagesBeforeCurrentPage) {
-            // current page near the start
-            startPage = 1;
-            endPage = maxPages;
-        } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
-            // current page near the end
-            startPage = totalPages - maxPages + 1;
-            endPage = totalPages;
-        } else {
-            // current page somewhere in the middle
-            startPage = currentPage - maxPagesBeforeCurrentPage;
-            endPage = currentPage + maxPagesAfterCurrentPage;
-        }
-    }
-
-    // calculate start and end item indexes
-    let startIndex = (currentPage - 1) * pageSize;
-    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-    // create an array of pages to ng-repeat in the pager control
-    // let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
-
-    // return object with all pager properties required by the view
-    return {
-        totalItems: totalItems,
-        currentPage: currentPage,
-        pageSize: pageSize,
-        totalPages: totalPages,
-        startPage: startPage,
-        endPage: endPage,
-        startIndex: startIndex,
-        endIndex: endIndex,
-        // pages: pages
-        setOfPages: setOfPages
-    };
-}
-
-// let itemsPerPage;
-// function getButtons(obj) {
-//     console.log(obj.setOfPages)
-//     let currentPage = obj.currentPage - 1;
-//     console.log(currentPage)
-//     let itemsPerPage = obj.pageSize;
-//     // Can be obtained from another source, such as your objJson variable
-
-//     document.getElementById("btn_next").addEventListener("click", function () {
-//         if (currentPage < obj.totalPages) {
-//             currentPage++;
-//             changePage(currentPage);
-//             console.log(currentPage)
-//             // console.log(obj)
-//             // return currentPage;
-
-//         }
-//     })
-//     document.getElementById("btn_prev").addEventListener("click", function () {
-//         if (currentPage) {
-//             currentPage--;
-//             changePage(currentPage);
-//             console.log(currentPage)
-//             // return currentPage;
-//         }
-//     })
-//     function changePage(page) {
-//         console.log(obj.setOfPages)
-//         let btnNext = document.getElementById("btn_next");
-//         let btnPrev = document.getElementById("btn_prev");
-//         let listing_table = document.getElementById("listingTable");
-//         let topCharts = document.getElementById("top-charts");
-//         let pageSpan = document.getElementById("page");
-
-//         // Validate page
-//         if (page < 1) page = 1;
-//         if (page > obj.totalPages) page = pageSize;
-
-//         // listing_table.innerHTML = "";
-
-//         // for (let i = (page - 1) * itemsPerPage; i < (page * itemsPerPage); i++) {
-//         //write to document
-//         // console.log(i)
-//         topCharts.innerHTML += `< table > ${ obj.setOfPages[currentPage].join("") }</table > `;
-//         // }
-//         pageSpan.innerHTML = page;
-
-//         if (page == 0) {
-//             btnPrev.style.visibility = "hidden";
-//         } else {
-//             btnPrev.style.visibility = "visible";
-//         }
-
-//         if (page == obj.totalPages) {
-//             btnNext.style.visibility = "hidden";
-//         } else {
-//             btnNext.style.visibility = "visible";
-//         }
-//     }
-
-
-
-
-//     console.log(currentPage)
-//     return currentPage;
-
-
-// }
 
