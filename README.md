3df4ef81d4fc2be25d5aa066de92caa4c2a3d2f2
COR1999 <cian.orourke@gmail.com>
2020-02-10 17:19:28 +0000

    cleaned up css and js file

--- README.md
+++ README.md
@@ -26,3 +26,43 @@ I decided to do this project on music cause i wanted to build off what i had fro
 How
 I acesses various endpoints in the lastFms api to get the data about albums i wanted and to implment a search bar so that the user could get the artist/album/song they where looking for.
 
+
+
+
+Your Project's Name
+One or two paragraphs providing an overview of your project.
+
+Essentially, this part is your sales pitch.
+
+UX
+In this project what i try to accive with my UX is a nice and simple way to find a small ammount of information on music.
+i have tried to make the colors easy to look at and the pictures as calming and relaxing as possible.
+the way i have layed out the website is basicly front page is to search for artist/albums/songs and the second page is too view the top 100.
+
+
+Features
+In this section, you should go over the different parts of your project, and describe each in a sentence or so.
+
+Existing Features
+Search by Artist/Ablum - Allows users to search the lastFm api using a simple search bar and they will then see a result of all the realted artist/albums for the keyword they input.
+
+View Album song's - Once you have searched for an artist or album you will then be able to click a button witch then presents you with a list of all songs on that album.
+
+View top 100 charts - Allows you to view the current top 100 songs sort them by Ranking, Name, Artist and Play Count. You can also search for the Song/Artist/PlayCount using the search bar.
+
+
+Features Left to Implement
+......
+
+
+Technologies Used
+.....
+Deployment
+.....
+Credits
+Content
+.....
+Media
+....
+Acknowledgements
+....
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,14 @@
 let baseUrl = "https://ws.audioscrobbler.com"
 let apiKey = "977e2d82b14fe34163d15b95f2c1ce55"
 let dropdownValue;
-let submit = document.getElementById('search'),
-    myInput = document.getElementById("myInput");
-let objJson;
+// let submit = document.getElementById('search');
+// let myInput = document.getElementById("myInput");
+let myInput = $("#myInput");
+let submit = $("#search")
 // let chartData = apiUrl("chart")
 
 $(document).ready(function () {
-
+    // Only Loads the chart function when the charts page has been loaded 
     window.onload = $(function () {
         if ($("body").is(".chart")) {
             getCharts()
@@ -17,18 +18,16 @@ $(document).ready(function () {
             // console.log(getCharts())
         }
     });
+    // Only listens for the submit on the Home page
     window.onload = $(function () {
         if ($("body").is(".index")) {
-            submit.addEventListener("click", function () {
+            submit.on("click", function () {
                 GetSelectedValue();
             });
             $(".artist-cards").html(`<div class="home-chart"><h1>Be sure to check out are top 100 page <a href="/chart.html" id="here-Link">here</a></h1></div>`)
         }
     })
-
-    let chart = document.getElementById("chart");
-    // chart.addEventListener("click", function () { getCharts() })
-
+    // Checks if the enter key is pressed in the input
     $('#myInput').keydown(function (event) {
         if (event.keyCode == '13') {
             $("#search").click();
@@ -37,15 +36,15 @@ $(document).ready(function () {
             //do nothing :)
         }
     });
+    // Gets a date to display on the chart's page
     let date = new Date();
-
     $("#date").html(date.toLocaleString())
     console.log(date)
 
 })
 
 
-
+// Get the selected value from the dropdown on the home page
 function GetSelectedValue() {
     let e = document.getElementById("option-menu");
     let result = e.options[e.selectedIndex].value;
@@ -61,8 +60,9 @@ function searchTheAPI(result) {
     let input;
     let searchType = result;
     console.log(searchType);
-
+    // Sets to input to equal the value in the input feild
     input = $("#myInput").val();
+    // if the Search Type equal album then searches the album endpoint and displays the data in cards
     if (searchType === "album") {
         apiUrl(searchType, input)
             .then(function (response) {
@@ -100,7 +100,7 @@ function searchTheAPI(result) {
 
 
             })
-    } else if (searchType === "song") {
+    } else if (searchType === "song") { // Checks to see if the dropdown value is Song and if it is goes to the songs endpoint and displays the data
         apiUrl(searchType, input)
             .then(function (response) {
                 let trackArray = response.results.trackmatches.track;
@@ -111,32 +111,21 @@ function searchTheAPI(result) {
                 trackArray.forEach(function (track, index) {
                     newTrackArray.push(`<div class="card songs-search col-sm-6 col-md-3 col-lg-2"><h4>${track.name}</h4><p>Artist: ${track.artist}<br>Total listens: ${(numberWithCommas(track.listeners))}</p></div>`)
                     if ((index + 1) % NumberPerRow === 0 || (index + 1) === trackArray.length) {
-                        // cardRow.push(`<div class="card">${albumImage}<h1>${album.artist}</h1><p class="title">${album.name}</p></div>`)
                         newTrackArrayRow.push(`<div class="row">${newTrackArray.join("")}</div>`);
                         newTrackArray.length = 0;
-                        // console.log(newTrackArrayRow)
                     }
-
                 })
-
-                // $(".artist-cards").html(`<div class="song-search-outer">${newTrackArrayRow}</div>`.replace(/,/g, ""));
                 $(".artist-cards").html(`<div class="song-search-outer"><h2>Search Results</h2>${newTrackArrayRow.join("")}</div>`);
-
-
-                // console.log(response.results.trackmatches);
             })
-    } else if (searchType === "chart") {
-        getCharts(searchType)
+        // Sets the drop down value to a empty string
+        dropdownValue = "";
     }
-    dropdownValue = "";
 }
+// Switch statement that checks to see if the input is album or song, or if they are on the chart's page then its set to chart
 function apiUrl(searchType, input) {
     let limit;
     let searchBy;
     switch (searchType) {
-        case "artist":
-            searchBy = "artist.search&artist=";
-            break;
         case "album":
             searchBy = "album.search&album=";
             limit = "20"
@@ -153,10 +142,7 @@ function apiUrl(searchType, input) {
             break;
         default:
             console.log("unkown Input", searchBy);
-        // code block
     }
-    // console.log("search ", searchType)
-
     return (
         $.ajax({
             "async": true,
@@ -166,7 +152,7 @@ function apiUrl(searchType, input) {
         }))
 }
 
-
+// Gets the song when u click on the album/artist card
 function getSongs(album, index) {
     let imageIndex = 2;
     let albumImage = `<img src="${album.image[imageIndex]["#text"]}">`
@@ -178,118 +164,28 @@ function getSongs(album, index) {
         "method": "GET",
     }).then(function (data) {
         let arrayOfTracks = data.album.tracks.track;
-
-        // console.log(data.album.tracks.track)
         let trackArray = [];
 
         arrayOfTracks.forEach(function (track) {
-            // console.log(track)
-            // console.log(track.name)
-            // console.log(album.name)
             $(`#view-songs${index}`).on("click", function () {
-                // console.log("index", index)
-                // console.log("track:", track.name)
-
                 trackArray.push(`<div class="card col-sm-6 col-md-4 col-lg-2" style="height:100px"><div class="track-name"><h4>${track.name}</h4></div><div class="track-duration"><p>Duration: ${turnSec(track.duration)}</p></div></div>`)
-
-                // console.log(trackArray);
-                // trackOuter.push(`<div  id="${index}"><p>${track.name}</p></div>`)
-                // console.log(arrayOfTracks)
-
                 $(".artist-cards").html(`<div class="album-songs">${albumImage}<div class="list-songs">${trackArray}</div></<div>`.replace(/,/g, ""))
             })
-            // if (track.name === arrayOfTracks) {
-            //     console.log(track.name)
-            // }
         })
     })
 }
-function getTableElement(el, value) {
-    // if (index === 0) { return };
 
-    let text = value;
-    if (el) {
-        text = el;
-    }
-    // console.log(el)
-    // console.log(text)
-    // console.log(value)
-    let str = `<td>${text}</td>`
-    return str
-}
-function chartHeaders() {
-    let tableHeader = ""
-    let tableNumber = `<th scope="col">Number</th>`,
-        tableSong = `<th scope="col">Song</th>`,
-        tableArtist = `<th scope="col">Artist</th>`,
-        tablePlaycount = `<th scope="col" >Play count</th>`;
-
-
-    tableHeader = tableNumber + tableSong + tableArtist + tablePlaycount
-
-    return `<thead><tr>${tableHeader}</tr></thead>`;
-}
-function chartRows(obj) {
-    let tableRows = [];
-    obj.forEach(function (track, index) {
-        let dataRow = [];
-
-        let newIndex = (getTableElement(index + 1));
-        let trackName = (getTableElement(track.name));
-        let artistName = (getTableElement(track.artist.name));
-        let playCount = (getTableElement(numberWithCommas(track.playcount)));
-        // str = newIndex + trackName + artistName + playCount
-        dataRow.push(newIndex)
-        dataRow.push(trackName);
-        dataRow.push(artistName);
-        dataRow.push(playCount);
-
-        // tableRows.push(`<tr>${str}</tr>`);
-
-        tableRows.push(`<tr >${dataRow.join("")}</tr>`);
-
-    });
-
-    let setOfPages = [];
-    // pageButtons(pageSize)
-    //     .then(function (l) {
-    //         console.log(l)
-    //     })
-    // console.log(tableRows.length)
-    // let pageSize = 10;
-    // let totalPages = Math.ceil(tableRows.length / pageSize)
-    // while (setOfPages.length < totalPages) {
-    //     setOfPages.push(tableRows.splice(0, pageSize))
-    // }
-    // console.log(setOfPages)
-    console.log(setOfPages)
-    // return tableRows.join("");
-    // return setOfPages
-
-    return tableRows
-}
-
+// Searches the api for the top 100 songs(cause i set it to only give back 100), then turns this response into arrays of 4 items, gives this data to the dataTables
+// and displays them nicely on the page for us
 function getCharts() {
-    let rows;
     let tableRows = [];
-    // console.log()
     apiUrl("chart")
         .then(function (response) {
-            // let objParsed = JSON.parse(response)
             let tracksArray = response.tracks.track;
             rows = chartRows(tracksArray);
-            // pageButtons(rows.length, rows)
-            // buildTable(rows, 1)
-            // let tableRows = [];
-            // let trackData = {
-            //     index: index,
-            //     name: name,
-
-            // }
             console.log(tracksArray[0].name)
             tracksArray.forEach(function (track, index) {
                 let dataRow = [];
-
                 let newIndex = index + 1
                 let trackName = track.name
                 let artistName = track.artist.name
@@ -302,7 +198,6 @@ function getCharts() {
                 tableRows.push(dataRow)
             })
             console.log(tableRows)
-            // console.log(rows)
             $("#chart-table").DataTable({
                 data: tableRows,
                 lengthChange: false,
@@ -321,63 +216,86 @@ function getCharts() {
                     { title: "Play Count" }
                 ]
             })
-            // let buildTable = `<table id="chart-table">${chartHeaders()}${rows}</table> `
-            // $(".top-charts").html(buildTable)
-
-            // $(".top-charts").html(tracksArray)
-            // return tableRows
         })
 
 }
 
-function buildTable(rows, page) {
-    // let header = chartHeaders()
-    // page = page - 1;
-    // console.log(page)
-    // pageSize = 10;
-    // let table = `<table id="chart-table">${header}${rows[page].join("")}</table> `
-    // $(".top-charts").html(table)
-    // console.log(page)
-    // $("#chart-table").DataTable();
-}
-
+// turns the given intager into seconds
 function turnSec(s) {
-    // let minutes = parseInt(s / 60) % 60;
-    // let seconds = s % 60;
     var minutes = ("00" + Math.floor((s % 3600) / 60)).slice(-2);
     let seconds = ("00" + (s % 3600) % 60).slice(-2);
     return minutes + ":" + seconds;
 
 }
+// divides a digit with a comma eg:3,540,343 = 3,540,343
 function numberWithCommas(num) {
     let numParts = num.toString().split(".");
     numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return numParts.join(".");
 }
 
-// function pageButtons(pages, rows) {
-//     rows = rows;
-//     let value;
-//     let wrapper = document.getElementById("buttons")
-//     wrapper.innerHTML = ""
-//     for (var page = 1; page <= pages; page++) {
-//         wrapper.innerHTML += `<button value="${page}" class="page-button" >${page}</button> `
+
+
+
+
+// function getTableElement(el, value) {
+
+//     let text = value;
+//     if (el) {
+//         text = el;
 //     }
-//     $(".page-button").on("click", function () {
-//         $(".top-charts").empty()
-//         value = $(this).val()
-//         // console.log(value)
-//         buildTable(rows, value)
-//         console.log(rows)
-//         // console.log(value)
-//     })
-// };
-// class Chart {
-//     constructor(index, song, name, playcount) {
-//         this.index = index;
-//         this.song = song;
-//         this.name = name;
-//         this.playcount = playcount;
-//     }
+//     let str = `<td>${text}</td>`
+//     return str
+// }
+// function chartHeaders() {
+//     let tableHeader = ""
+//     let tableNumber = `<th scope="col">Number</th>`,
+//         tableSong = `<th scope="col">Song</th>`,
+//         tableArtist = `<th scope="col">Artist</th>`,
+//         tablePlaycount = `<th scope="col" >Play count</th>`;
+
+
+//     tableHeader = tableNumber + tableSong + tableArtist + tablePlaycount
+
+//     return `<thead><tr>${tableHeader}</tr></thead>`;
+// }
+// function chartRows(obj) {
+//     let tableRows = [];
+//     obj.forEach(function (track, index) {
+//         let dataRow = [];
+
+//         let newIndex = (getTableElement(index + 1));
+//         let trackName = (getTableElement(track.name));
+//         let artistName = (getTableElement(track.artist.name));
+//         let playCount = (getTableElement(numberWithCommas(track.playcount)));
+//         // str = newIndex + trackName + artistName + playCount
+//         dataRow.push(newIndex)
+//         dataRow.push(trackName);
+//         dataRow.push(artistName);
+//         dataRow.push(playCount);
+
+//         // tableRows.push(`<tr>${str}</tr>`);
+
+//         tableRows.push(`<tr >${dataRow.join("")}</tr>`);
+
+//     });
+
+//     let setOfPages = [];
+//     // pageButtons(pageSize)
+//     //     .then(function (l) {
+//     //         console.log(l)
+//     //     })
+//     // console.log(tableRows.length)
+//     // let pageSize = 10;
+//     // let totalPages = Math.ceil(tableRows.length / pageSize)
+//     // while (setOfPages.length < totalPages) {
+//     //     setOfPages.push(tableRows.splice(0, pageSize))
+//     // }
+//     // console.log(setOfPages)
+//     console.log(setOfPages)
+//     // return tableRows.join("");
+//     // return setOfPages
+
+//     return tableRows
 // }
 
